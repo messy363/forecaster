50898ad214e0f87c8a94130a764e48e53c9f4f4a
messy363 <messynick@gmail.com>
2025-07-20 21:47:40 +0100

    First Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsPzWnUA-UV8TVPqZwnWbhjilZskbq4Y4",
+  authDomain: "my-financial-forecaster.firebaseapp.com",
+  projectId: "my-financial-forecaster",
+  storageBucket: "my-financial-forecaster.firebasestorage.app",
+  messagingSenderId: "184308464699",
+  appId: "1:184308464699:web:54b61bb1789d490d2d701e"
+};
